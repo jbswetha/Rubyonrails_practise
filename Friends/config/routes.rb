0f4c47fb6404cbcc2cc4db87e4c55ec788d2e382
jbswetha <jbswetha@gmail.com>
2021-07-16 19:08:23 +0530

    association and styling

--- Friends/config/routes.rb
+++ Friends/config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'hom/ind'
   #get 'home/index'
   get 'home/about'
- root 'home#index'
+ #root 'home#index'
+ root 'friends#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
